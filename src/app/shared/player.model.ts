b83e7a500a358bdb02ff4277f0fc5381fb393013
Jasper Luiken <jasper@3po.nl>
2017-09-20 22:03:16 +0200

    Add name parameter in constructor

--- src/app/shared/player.model.ts
+++ src/app/shared/player.model.ts
@@ -2,4 +2,8 @@ export class Player {
     name: string;
     isPresent: boolean = true ;
     doNotSubstitute: boolean = false;
+    
+    constructor(name:string) {
+        this.name = name;
+    }
 }
